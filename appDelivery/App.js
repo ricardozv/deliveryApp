7f63f69f35845c1ce331fad3dc5c7cdd1aac0f81
zumb <zumb@zumb.zumb>
2022-04-21 03:16:53 -0300

    create listItem

--- appDelivery/App.js
+++ appDelivery/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Home from './src/screens/Home/home';
 import CompanyDetais from './src/screens/CompanyDetails/companyDetails';
-
 export default function App() {
   return (
     <View style={styles.container}>
