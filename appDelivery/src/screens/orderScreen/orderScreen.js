52de7e7eb0451befa088c25331e02d9e484eba94
zumb <zumb@zumb.zumb>
2022-04-25 13:41:53 -0300

    create orderScreen on screens

---
+++ appDelivery/src/screens/orderScreen/orderScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const orderScreen = () => {
+    return (
+        <View>
+            <Text>
+                orderScreen
+            </Text>
+        </View>
+    )
+}
+
+export default orderScreen;
